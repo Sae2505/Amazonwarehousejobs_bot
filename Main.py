4dbc9387e17d2ee210d2286e81225e97794c1b78
Sae2505 <mendusai25@gmail.com>
2026-06-07 01:30:32 +0100

    Create Main.py
    Telegram bot for Amazon UK warehouse job alerts

---
+++ Main.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = "Amazon Jobs Bot is running."
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
