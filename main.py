6c2a6dfe7f0568b70a4a9729853e14b1c66b9681
Sae2505 <mendusai25@gmail.com>
2026-06-07 02:47:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ CHAT_ID = os.environ["CHAT_ID"]
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-response = requests.get(url, params={
+r = requests.get(url, params={
     "chat_id": CHAT_ID,
-    "text": "🔥 Bot test working from GitHub Actions"
+    "text": "TEST FROM GITHUB ACTION 🚀"
 })
 
-print(response.status_code)
-print(response.text)
+print("STATUS CODE:", r.status_code)
+print("RESPONSE:", r.text)
